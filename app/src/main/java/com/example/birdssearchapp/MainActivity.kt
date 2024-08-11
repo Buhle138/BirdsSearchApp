61a0dd03fd5b115ca06e5b6ae62ce095d2291508
buhleradzilani <radzilanibuhle@gmail.com>
2024-08-11 21:54:25 +0200

    adding the map options xml

--- app/src/main/java/com/example/birdssearchapp/MainActivity.kt
+++ app/src/main/java/com/example/birdssearchapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.birdssearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private  var mGoogleMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
+
+        val mapOptionButton: ImageButton = findViewById(R.id.mapOptionsMenu)
+
+        val popupMenu = PopupMenu(this, mapOptionButton)
+
+        popupMenu.menuInflater.inflate(R.menu.map_options, popupMenu.menu )
+
+
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        mGoogleMap = googleMap
     }
 }
